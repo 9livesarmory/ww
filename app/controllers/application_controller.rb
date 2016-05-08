160b13c145eb68c2aecd57ce25699053e38c1cbe
Michelle Muzyka <9livesarmory@gmail.com>
2016-05-08 00:10:36 -0400

    Added search index route and page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
-		devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name]
+		devise_parameter_sanitizer.for(:account_update) << [:first_name, :last_name, :phone]
 	end
 end
