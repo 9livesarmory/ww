a606854f6c7add0b7e2582126cca211e0e3dcb23
Michelle Muzyka <9livesarmory@gmail.com>
2016-05-16 12:47:20 -0400

    removed signup btns for prototype deployment

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -6,27 +6,27 @@ $(document).ready(function() {
     //$('.js-calcPrice').focusOut(showFinalPrice);
   });
 
-function showFinalPrice () {
-		// var quantity = event.currentTarget;
-		// console.log(currentTarget);
+// function showFinalPrice () {
+// 		// var quantity = event.currentTarget;
+// 		// console.log(currentTarget);
 
-	// if (bottom_total !== nil) {
-	// 	bottom_total = parseInt($('#bottom_quantity').val());
-	// }
-	// else {
-	// 	bottom_total=0;
-	// }
-	// var top_quantity = parseInt($('#top_quantity').val());
-	// var bottom_quantity = parseInt($('#bottom_quantity').val());
-	// var dress_quantity = parseInt($('#dress_quantity').val());
-	// var totalprice = (bottom_total + tops_total + dresses_total);
-	var totalprice = 10;
+// 	// if (bottom_total !== nil) {
+// 	// 	bottom_total = parseInt($('#bottom_quantity').val());
+// 	// }
+// 	// else {
+// 	// 	bottom_total=0;
+// 	// }
+// 	// var top_quantity = parseInt($('#top_quantity').val());
+// 	// var bottom_quantity = parseInt($('#bottom_quantity').val());
+// 	// var dress_quantity = parseInt($('#dress_quantity').val());
+// 	// var totalprice = (bottom_total + tops_total + dresses_total);
+// 	var totalprice = 10;
 
-	var html = `
-				<div>
-					<h5>Total: ${totalprice}</h5>
-				</div>`;
+// 	var html = `
+// 				<div>
+// 					<h5>Total: ${totalprice}</h5>
+// 				</div>`;
 	
-	$('.js-total-price').append(html);
+// 	$('.js-total-price').append(html);
 
-}
+// }
